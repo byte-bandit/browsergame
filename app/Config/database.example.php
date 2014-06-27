0e55bfd59eac50094fd7273d1890973f94776c93
acrynx <acrynx@googlemail.com>
2014-06-27 08:41:09 +0200

    Update and rename database.php to database.example.php

---
+++ app/Config/database.example.php
@@ -0,0 +1,10 @@
+<?php
+class DatabaseConfig
+{
+    public static $default = array(
+        'host'     => 'www.example.com',
+        'username' => 'mysql_user',
+        'password' => 'password123',
+        'database' => 'browsergame',
+    );
+}
